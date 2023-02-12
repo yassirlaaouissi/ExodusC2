cd3c58bd865a797495214d525e33d9405725cc51
Yassir Laaouissi <yassirlaaouissi1@gmail.com>
2023-02-12 17:51:37 +0100

    debug  2

--- beacon.c
+++ beacon.c
@@ -62,9 +62,9 @@ void beacon_connect_to_server(char IP[16], int PORT){
     char recvbuf[recvbuflen];
 
     // base64 encode the UUID of the beacon
-    printf("\nBASE64\n");
     char b64size[1024];
     DWORD strcount = _countof(b64size);
+    printf("\nBASE64\n");
     char *UUID_BUFFER;
     BOOL res = CryptBinaryToStringA(sendbuf, 128, 1, UUID_BUFFER, &strcount);
 
